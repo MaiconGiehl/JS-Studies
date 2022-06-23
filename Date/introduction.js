bcabc16a920358164e5d6e7a819c8d95e9b700b3
Maicon Giehl <maicongiehl@gmail.com>
2022-06-23 17:12:05 -0300

    more-date-manipulation

--- Date/introduction.js
+++ Date/introduction.js
@@ -1,5 +1,6 @@
 const date = new Date();
-console.log(date);
+
+date.setDate(5);
 console.log("day: " + date.getDay());
 console.log("date: " + date.getDate());
 console.log("Month: " + date.getMonth());
@@ -9,5 +10,24 @@ console.log("Hours: " + date.getHours());
 console.log("UTC-Hours: " + date.getUTCHours());
 console.log("Time Off-Set: " + date.getTimezoneOffset());
 
-date.setDate(5);
-console.log(date)
+
+// to string
+
+console.log("");
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+console.log(date.toUTCString());
+
+// format
+console.log("");
+console.log(date.toLocaleString("en-US"));
+console.log(date.toLocaleString("en-US", {month: "long", weekday: "long", day: "numeric"}));
+console.log(date.toLocaleString("pt-BR", {month: "long", weekday: "long", day: "numeric"}));
+console.log(date.toLocaleString("pt-BR", {month: "short", weekday: "short", day: "numeric"}));
+
+
+console.log("");
+console.log(date);
